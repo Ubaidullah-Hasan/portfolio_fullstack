cbc1c14616cbb204de70d8059ed4e9841d1cbb49
Ubaidullah-Hasan <hm2964133@gmail.com>
2025-01-18 22:56:18 +0600

    update

--- src/app/modules/skill/skill.model.ts
+++ src/app/modules/skill/skill.model.ts
@@ -3,7 +3,7 @@ import { ISkill } from './skill.interface';
 
 const skillSchema = new Schema<ISkill>(
     {
-        name: { type: String, required: true },
+        name: { type: String, required: true, unique: true  },
         percentage: { type: Number, required: true, min: 0, max: 100 },
         description: { type: String, required: true },
         image: { type: String, required: true},
